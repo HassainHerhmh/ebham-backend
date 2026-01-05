f50bb490a42eb3326bc9a25a7c994263bae6b7c5
HassainHerhmh <aabcaade6@gmail.com>
2026-01-05 20:36:35 +0300

    Update db.js

--- db.js
+++ db.js
@@ -3,13 +3,10 @@ const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-
-  // 👇 هذا هو الحل
-  family: 4, // Force IPv4
 });
 
-
-
+pool.query("select 1")
+  .then(() => console.log("✅ DB CONNECTED (Railway PostgreSQL)"))
+  .catch(err => console.error("❌ DB ERROR:", err.message));
 
 export default pool;
