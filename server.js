f09a4290918ca5f11db30f4b20f3ec6489be3d03
HassainHerhmh <aabcaade6@gmail.com>
2026-01-05 17:30:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -21,8 +21,17 @@ app.use(
       "http://localhost:5173",
     ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+    ],
   })
 );
+
+// مهم جدًا
+app.options("*", cors());
 
 /* =========================
    Login (حقيقي)
