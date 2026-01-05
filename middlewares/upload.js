59cb4f6151bdd60814d3d8ad1255a1af076b3aa6
HassainHerhmh <aabcaade6@gmail.com>
2026-01-06 01:32:00 +0300

    Create upload.js

---
+++ middlewares/upload.js
@@ -0,0 +1,30 @@
+import multer from "multer";
+import path from "path";
+
+// تخزين الصور
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    cb(null, Date.now() + ext);
+  },
+});
+
+// فلترة الملفات (صور فقط)
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true);
+  } else {
+    cb(new Error("الملف يجب أن يكون صورة"), false);
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: { fileSize: 2 * 1024 * 1024 }, // 2MB
+});
+
+export default upload;
