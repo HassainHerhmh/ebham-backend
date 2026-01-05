80cae034f84fdc3fc3e998c5763ebacf78ce4983
HassainHerhmh <aabcaade6@gmail.com>
2026-01-05 21:44:43 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,15 +1,6 @@
 import mysql from "mysql2/promise";
 
-const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: Number(process.env.MYSQLPORT),
-
-  waitForConnections: true,
-  connectionLimit: 10,
-});
+const pool = mysql.createPool(process.env.MYSQL_URL);
 
 (async () => {
   try {
