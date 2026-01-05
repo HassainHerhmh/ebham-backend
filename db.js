773d11efaeac93a74c4800f9a769eddb8d5cdcee
HassainHerhmh <aabcaade6@gmail.com>
2026-01-05 21:03:28 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,32 @@
-import pkg from "pg";
-const { Pool } = pkg;
+import mysql from "mysql2/promise";
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+/* =========================
+   🛢️ MySQL Connection Pool
+========================= */
+const pool = mysql.createPool({
+  host: process.env.MYSQLHOST  ||  mysql.railway.internal, 
+  user: process.env.MYSQLUSER   ||  root,  
+  password: process.env.MYSQLPASSWORD  || uJUVWhgygtvQoXDgsAQxjUfLEPWXGGHC,
+  database: process.env.MYSQLDATABASE ||  railway, 
+  port: process.env.MYSQLPORT || 3306,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-pool.query("select 1")
-  .then(() => console.log("✅ DB CONNECTED (Railway PostgreSQL)"))
-  .catch(err => console.error("❌ DB ERROR:", err.message));
+/* =========================
+   🔌 Test Connection
+========================= */
+(async () => {
+  try {
+    const conn = await pool.getConnection();
+    await conn.query("SELECT 1");
+    conn.release();
+    console.log("✅ MySQL CONNECTED SUCCESSFULLY");
+  } catch (err) {
+    console.error("❌ MYSQL CONNECTION ERROR:", err.message);
+  }
+})();
 
 export default pool;
