475eb499737a7680c54268bd9645613926940ee6
HassainHerhmh <aabcaade6@gmail.com>
2026-01-08 12:00:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,20 +15,10 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: [
-      "https://ebham-dashboard2.vercel.app", // لوحة التحكم
-      "https://ebham-apk.vercel.app",        // صفحة العملاء
-      "http://localhost:5173",
-    ],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "X-Requested-With",
-    ],
+    origin: "*",
   })
 );
+
 
 // مهم جدًا
 app.options("*", cors());
