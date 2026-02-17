cafef5002f3a2f7810c4465240c361ba6ab017e6
HassainHerhmh <aabcaade6@gmail.com>
2026-02-17 08:39:05 +0300

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,8 +1,11 @@
 import admin from "firebase-admin";
-import serviceAccount from "./serviceAccountKey.json" assert { type: "json" };
+
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount)
 });
 
 export default admin;
