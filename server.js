21dfeb6f2dd61b9172e9dbdccb426924b7b66bef
HassainHerhmh <aabcaade6@gmail.com>
2026-03-06 14:46:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -164,14 +164,13 @@ const corsOptions = {
 
   credentials: true,
 
-methods: [
-  "GET",
-  "POST",
-  "PUT",
-  "PATCH",
-  "DELETE",
-  "OPTIONS"
-]
+  methods: [
+    "GET",
+    "POST",
+    "PUT",
+    "DELETE",
+    "OPTIONS"
+  ],
 
   allowedHeaders: [
     "Content-Type",
